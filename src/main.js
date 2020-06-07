0005a16e28a8b44602a7c48e5db06a512e4c6a5d
Kai Weber <sermo_de_arboribus@seznam.cz>
2020-06-07 21:54:06 +0200

    prepare for i18n

--- src/main.js
+++ src/main.js
@@ -1,20 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
+import Vue from "vue"
+import App from "./App.vue"
+import { BootstrapVue } from "bootstrap-vue"
+//import VueI18n from "vue-i18n"
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import router from './router'
-import store from './store'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import "@fortawesome/fontawesome-free/css/all.css"
+import router from "./router"
+import store from "./store"
+
+// call with I18n option
+/*const messages = {
+    cs: {  },
+    de: {  },
+    en: {  },
+    zh: {  }
+};*/
 
 Vue.config.productionTip = false
 
+/*const i18n = new VueI18n({
+  locale: "de",
+  messages
+});*/
+
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+//Vue.use(i18n);
 
 new Vue({
+//  i18n,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const additions = require("../data/additions.json");
 const axios = require("axios").default;
 
 export default new Vuex.Store({
   state: {
-    allMoersFestivalEvents: null
+    allMoersFestivalEvents: null,
+    useRemoteApi: true
   },
   mutations: {
     updateEvents(state, data) {
@@ -16,27 +17,33 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    fetchEventsFromApi({ commit }) {
-      axios.get("https://meinmoers.lambdadigamma.com/api/v2/moers-festival/events/all")
-        .then(function (response) {
-          let events = null;
+    fetchEventsFromApi({ commit, state }) {
 
-          if(response.status > 399) {
-              events = require("../data/backup.json");
-          } else {
-              events = response.data.map(ev => {
-                  const eventId = ev.id;
-                  if(additions[eventId]) {
-                      return Object.assign(ev, additions[eventId]);
-                  } else {
-                      return ev;
-                  }
-              })
-          }
-          commit("updateEvents", events);
-      })
+      let events = null;
+
+      if(state.useRemoteApi) {
+        axios.get("https://meinmoers.lambdadigamma.com/api/v2/moers-festival/events/all")
+            .then(function (response) {
+              events = addAdditionalData(response.data);
+              commit("updateEvents", events);
+          })
+      } else {
+        events = addAdditionalData(require("../data/backup.json"));
+        commit("updateEvents", events);
+      }
     }
   },
   modules: {
   }
+});
+
+function addAdditionalData(events) {
+  return events.map(ev => {
+    const eventId = ev.id;
+    if(additions[eventId]) {
+        return Object.assign(ev, additions[eventId]);
+    } else {
+        return ev;
+    }
 })
+}
