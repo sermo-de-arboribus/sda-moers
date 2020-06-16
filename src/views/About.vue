389f27ac6a260b5211812ea40dddb67130baba8d
Kai Weber <sermo_de_arboribus@seznam.cz>
2020-06-16 22:47:57 +0200

    i18n: translated first half of the imprint page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="about container">
-    <h1>Impressum</h1>
-    <p>Dies ist eine private Website von <br/>
-      <br/>
-       Kai Weber<br/>
-       Superzuschauer des mœrs festival 2020<br/>
-       Mühleweg 7<br/>
-       72138 Kirchentellinsfurt<br/>
-       sermo_de_arboribus[das-At-Zeichen]seznam.cz
-    </p>
-    <img src="../assets/Moers-Superzuschauer.jpg" style="width:50%"/>
-    <h2>Datenschutzerklärung</h2>
-    <p>Diese Website verwendet keinerlei Cookies und speichert keine persönliche Daten.</p>
-    <p>Auf dem Webserver kurzfristig in Log-Dateien abgelegte IP-Adressen werden spätestens binnen 14 Tagen gelöscht.</p>
-    <p>Für weitere Fragen zum Datenschutz wenden Sie sich bitte an die unter „Impressum“ genannten Kontaktdaten.</p>
-    <h2>Quellcode</h2>
-    <p>Der Quellcode dieser Seite steht auf <a href="https://github.com/sermo-de-arboribus/sda-moers">Github</a> zur Verfügung.</p>
+    <section>
+      <h1>{{ $t("aboutPage.imprint") }}</h1>
+      <p>{{ $t("aboutPage.privateWebsiteOf") }}<br/>
+        <br/>
+         Kai Weber<br/>
+         {{ $t("aboutPage.superViewer") }}<br/>
+         Mühleweg 7<br/>
+         72138 Kirchentellinsfurt<br/>
+         {{ $t("aboutPage.germany")}}<br/>
+         sermo_de_arboribus[{{$t("aboutPage.atSign")}}]seznam.cz
+      </p>
+      <img src="../assets/Moers-Superzuschauer.jpg" style="width:50%"/>
+    </section>
+    <section>
+      <h2>{{ $t("aboutPage.privacyHeader")}}</h2>
+      <p>{{$t("aboutPage.privacyLine1")}}</p>
+      <p>{{$t("aboutPage.privacyLine2")}}</p>
+      <p>{{$t("aboutPage.privacyLine3")}}</p>
+    </section>
+    <section>
+      <h2>Quellcode</h2>
+      <p>Der Quellcode dieser Seite steht auf <a href="https://github.com/sermo-de-arboribus/sda-moers">Github</a> zur Verfügung.</p>
+    </section>
     <hr/>
-    <h3>Rechtliche Hinweise</h3>
-    <p>© 2020 Kai Weber</p>
-    <p>moers festival and mœrs festival are trademarks of Moers Kultur GmbH</p>
-    <p>The logos of All About Jazz, Allmusic, Bandcamp, Discogs, Soundcloud, Twitter, and Youtube are trademarks of the respective companies that run those websites.</p>
-    <p>All other logos are open source logos by <a href="https://fontawesome.com/">Font Awesome</a>.</p>
-    <p>This website uses the JavaScript framework <a href="https://vuejs.org/">Vue.js</a> and the <a
+    <section>
+      <h3>Rechtliche Hinweise</h3>
+      <p>© 2020 Kai Weber</p>
+      <p>moers festival and mœrs festival are trademarks of Moers Kultur GmbH</p>
+      <p>The logos of All About Jazz, Allmusic, Bandcamp, Discogs, Soundcloud, Twitter, and Youtube are trademarks of the respective companies that run those websites.</p>
+      <p>All other logos are open source logos by <a href="https://fontawesome.com/">Font Awesome</a>.</p>
+      <p>This website uses the JavaScript framework <a href="https://vuejs.org/">Vue.js</a> and the <a
         href="https://xaksis.github.io/vue-good-table/">vue-good-table</a> library.</p>
+    </section>
   </div>
 </template>
+
+<style>
+section {
+  margin-top: 1em;
+}
+</style>
