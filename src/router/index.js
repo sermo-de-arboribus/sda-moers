fa637dc12b2046c8156aca3655e9ad591680548c
Kai Weber <kai.weber@pagina.gmbh>
2020-07-15 13:24:35 +0200

    Fixed bug with about page

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,18 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/:perPage?/:page?',
-    name: 'Home',
-    props: true,
-    component: ArtistsTable
-  },
-  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/:perPage?/:page?',
+    name: 'Home',
+    props: true,
+    component: ArtistsTable
   }
 ]
 
